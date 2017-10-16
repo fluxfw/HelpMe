d2da9f720de8d9ef8205c6df40a960fbabbad88b
Florian <fw@studer-raimann.ch>
2017-10-16 13:13:26 +0200

    Plugin configuration form

--- classes/class.ilHelpMeConfigGUI.php
+++ classes/class.ilHelpMeConfigGUI.php
@@ -1,11 +1,45 @@
 <?php
 
 require_once "Services/Component/classes/class.ilPluginConfigGUI.php";
+require_once "Services/Form/classes/class.ilPropertyFormGUI.php";
+require_once "Services/Form/classes/class.ilRadioGroupInputGUI.php";
+require_once "Services/Form/classes/class.ilRadioOption.php";
+require_once "Services/Form/classes/class.ilTextInputGUI.php";
+require_once "Services/Form/classes/class.ilTextAreaInputGUI.php";
+require_once "Services/Form/classes/class.ilMultiSelectInputGUI.php";
 
 /**
  *
  */
 class ilHelpMeConfigGUI extends ilPluginConfigGUI {
+
+	/**
+	 * @var ilCtrl
+	 */
+	protected $ctrl;
+	/**
+	 * @var ilHelpMeUIHookGUI
+	 */
+	protected $pl;
+	/**
+	 * @var ilTemplate
+	 */
+	protected $tpl;
+
+
+	function __construct() {
+		/**
+		 * var ilCtrl $ilCtrl
+		 * var ilTemplate $tpl
+		 */
+
+		global $ilCtrl, $tpl;
+
+		$this->ctrl = $ilCtrl;
+		$this->pl = ilHelpMePlugin::getInstance();
+		$this->tpl = $tpl;
+	}
+
 
 	/**
 	 *
@@ -14,6 +48,7 @@ class ilHelpMeConfigGUI extends ilPluginConfigGUI {
 	function performCommand($cmd) {
 		switch ($cmd) {
 			case "configure":
+			case "updateConfigure":
 				$this->$cmd();
 				break;
 
@@ -25,8 +60,92 @@ class ilHelpMeConfigGUI extends ilPluginConfigGUI {
 
 	/**
 	 *
+	 * @return ilPropertyFormGUI
 	 */
-	function configure() {
+	protected function getConfigurationForm() {
+		$form = new ilPropertyFormGUI();
 
+		$form->setFormAction($this->ctrl->getFormAction($this));
+
+		$form->setTitle($this->txt("srsu_configuration"));
+
+		$form->addCommandButton("updateConfigure", $this->txt("srsu_update"));
+
+		$recipient = new ilRadioGroupInputGUI($this->txt("srsu_recipient"), "srsu_recipient");
+		$recipient->setRequired(true);
+
+		$recipient_email = new ilRadioOption();
+		$recipient_email->setTitle($this->txt("srsu_send_email"));
+		$recipient_email->setValue("send_mail");
+
+		$send_email_address = new ilTextInputGUI($this->txt("srsu_email_address"), "srsu_send_email_address");
+		$send_email_address->setRequired(true);
+		$recipient_email->addSubItem($send_email_address);
+
+		$recipient->addOption($recipient_email);
+
+		$recipient_jira = new ilRadioOption();
+		$recipient_jira->setTitle($this->txt("srsu_create_jira_ticket"));
+		$recipient_jira->setDisabled(true);
+		$recipient_jira->setValue("create_jira_ticket");
+		$recipient->addOption($recipient_jira);
+
+		$recipient->setValue("send_mail");
+
+		$form->addItem($recipient);
+
+		$info = new ilTextAreaInputGUI($this->txt("srsu_info"), "srsu_info");
+		$info->setRequired(true);
+		$form->addItem($info);
+
+		$priority = new ilTextAreaInputGUI($this->txt("srsu_priority"), "srsu_priority");
+		$priority->setRequired(true);
+		$form->addItem($priority);
+
+		$roles = new ilMultiSelectInputGUI($this->txt("srsu_role"), "srsu_role");
+		$roles->setRequired(true);
+		$form->addItem($roles);
+
+		return $form;
+	}
+
+
+	/**
+	 *
+	 */
+	protected function configure() {
+		$form = $this->getConfigurationForm();
+
+		$this->tpl->setContent($form->getHTML());
+	}
+
+
+	protected function updateConfigure() {
+		$form = $this->getConfigurationForm();
+		$form->setValuesByPost();
+
+		if (!$form->checkInput()) {
+			$this->tpl->setContent($form->getHTML());
+
+			return;
+		}
+
+		$recipient = $form->getInput("srsu_recipient");
+		$send_email_address = $form->getInput("srsu_send_email_address");
+		$info = $form->getInput("srsu_info");
+		$priority = $form->getInput("srsu_priority");
+		$role = $form->getInput("srsu_role");
+
+		$this->tpl->setContent($form->getHTML());
+	}
+
+
+	/**
+	 * @param string $a_var
+	 *
+	 * @return string
+	 */
+	protected function txt($a_var) {
+		return $this->pl->txt($a_var);
 	}
 }
