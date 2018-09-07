1d4a1b57dd1e8d8d4ba69cef02faf6249b1fe2d9
Florian <fw@studer-raimann.ch>
2018-09-07 09:33:10 +0200

    Fix

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -11,20 +11,20 @@
 \srag\Plugins\HelpMe\Config\HelpMeConfig::updateDB();
 
 if (\srag\DIC\DICStatic::dic()->database()->tableExists(\srag\Plugins\HelpMe\Config\HelpMeConfigOld::TABLE_NAME)) {
-	$config = \srag\Plugins\HelpMe\Config\HelpMeConfigOld::getConfig();
+	$config_old = \srag\Plugins\HelpMe\Config\HelpMeConfigOld::getConfig();
 
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setInfo($config->getInfo());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraAccessToken($config->getJiraAccessToken());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraAuthorization($config->getJiraAuthorization());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraConsumerKey($config->getJiraConsumerKey());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraDomain($config->getJiraDomain());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraIssueType($config->getJiraIssueType());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraPassword($config->getJiraPassword());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraPrivateKey($config->getJiraPrivateKey());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraProjectKey($config->getJiraProjectKey());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraUsername($config->getJiraUsername());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setRecipient($config->getRecipient());
-	\srag\Plugins\HelpMe\Config\HelpMeConfig::setSendEmailAddress($config->getSendEmailAddress());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setInfo($config_old->getInfo());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraAccessToken($config_old->getJiraAccessToken());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraAuthorization($config_old->getJiraAuthorization());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraConsumerKey($config_old->getJiraConsumerKey());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraDomain($config_old->getJiraDomain());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraIssueType($config_old->getJiraIssueType());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraPassword($config_old->getJiraPassword());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraPrivateKey($config_old->getJiraPrivateKey());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraProjectKey($config_old->getJiraProjectKey());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setJiraUsername($config_old->getJiraUsername());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setRecipient($config_old->getRecipient());
+	\srag\Plugins\HelpMe\Config\HelpMeConfig::setSendEmailAddress($config_old->getSendEmailAddress());
 
 	\srag\DIC\DICStatic::dic()->database()->dropTable(\srag\Plugins\HelpMe\Config\HelpMeConfigOld::TABLE_NAME);
 }
