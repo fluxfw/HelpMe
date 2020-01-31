1f284b4d046d64000f6ba0d34af5d13c6701496b
Florian Wyss <fw@studer-raimann.ch>
2020-01-31 11:27:12 +0100

    Fix

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,13 +6,13 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'srag\\RequiredData\\' => array($vendorDir . '/srag/requireddata/src'),
+    'srag\\RequiredData\\HelpMe\\' => array($vendorDir . '/srag/requireddata/src'),
     'srag\\RemovePluginDataConfirm\\HelpMe\\' => array($vendorDir . '/srag/removeplugindataconfirm/src'),
     'srag\\Plugins\\HelpMe\\' => array($baseDir . '/src'),
     'srag\\Notifications4Plugin\\HelpMe\\' => array($vendorDir . '/srag/notifications4plugin/src'),
     'srag\\LibrariesNamespaceChanger\\' => array($vendorDir . '/srag/librariesnamespacechanger/src'),
     'srag\\JiraCurl\\HelpMe\\' => array($vendorDir . '/srag/jiracurl/src'),
     'srag\\DIC\\HelpMe\\' => array($vendorDir . '/srag/dic/src'),
-    'srag\\CustomInputGUIs\\HelpMe\\' => array($vendorDir . '/srag/custominputguis/src'),
-    'srag\\ActiveRecordConfig\\HelpMe\\' => array($vendorDir . '/srag/activerecordconfig/src'),
+    'srag\\CustomInputGUIs\\' => array($vendorDir . '/srag/custominputguis/src'),
+    'srag\\ActiveRecordConfig\\' => array($vendorDir . '/srag/activerecordconfig/src'),
 );
