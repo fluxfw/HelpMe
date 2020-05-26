63b3a696bde12d4a48fa057cccd6c892a0add51e
Florian Wyss <fw@studer-raimann.ch>
2020-05-26 10:50:04 +0200

    Fix escaped html line breaks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [6.0.6]
+- Disable submit button on submit for prevent multiple support requests
+- Fix escaped html line breaks
+
 ## [6.0.5]
 - Fix
 
--- vendor/srag/notifications4plugin/src/Parser/twigParser.php
+++ vendor/srag/notifications4plugin/src/Parser/twigParser.php
@@ -46,7 +46,8 @@ class twigParser extends AbstractParser
                 "setInfo"                       => nl2br(implode("\n", [
                     self::notifications4plugin()->getPlugin()->translate("parser_option_autoescape_info_1", NotificationsCtrl::LANG_MODULE, ["|raw"]),
                     self::notifications4plugin()->getPlugin()->translate("parser_option_autoescape_info_2", NotificationsCtrl::LANG_MODULE, ["|e"]),
-                    "<b>" . self::notifications4plugin()->getPlugin()->translate("parser_option_autoescape_info_3", NotificationsCtrl::LANG_MODULE) . "</b>"
+                    "<b>" . self::notifications4plugin()->getPlugin()->translate("parser_option_autoescape_info_3", NotificationsCtrl::LANG_MODULE) . "</b>",
+                    self::notifications4plugin()->getPlugin()->translate("parser_option_autoescape_info_4", NotificationsCtrl::LANG_MODULE)
                 ]), false)
             ]
         ];
@@ -66,6 +67,6 @@ class twigParser extends AbstractParser
             "autoescape" => boolval($options["autoescape"])
         ]);
 
-        return $twig->render($text, $placeholders);
+        return $this->fixLineBreaks($twig->render($text, $placeholders));
     }
 }
